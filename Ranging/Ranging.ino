0acc6fafcb97bcbd22d37ff9585f5c4adef133b9
onkar406 <onkar406@aol.com>
2014-08-23 23:44:25 +0530

    Revert "Revert "Changes on Group Meeting""
    This reverts commit bc1715c47cc50494584b735b9b80b004e89e4c1f.

--- Ranging/Ranging.ino
+++ Ranging/Ranging.ino
@@ -1,10 +1,14 @@
+#include<Servo.h>
+
 int echoPin = 6;  // digital pin to receive echo pulse
 int triggerPin = 7;  // digital pin to send trigger pulse
 unsigned long distance = 0;
-
+Servo myServo;
+int pos=0;
 void setup()
 {  
   Serial.begin(9600);
+  myServo.attach(9);
   pinMode(echoPin, INPUT);
   pinMode(triggerPin, OUTPUT);
 }
@@ -18,5 +22,27 @@ void loop()
   distance = distance / 58; // calculate distance from pulse length
   Serial.println(distance, DEC);
   delay(50);  // wait 50ms for next ranging
+
+  if(distance<40)
+  {
+    Serial.println("Left");
+    myServo.write(60);
+    delay(50);
+
+  }
+  else if(distance>40)
+  {
+    Serial.println("Right");
+    myServo.write(120);
+    delay(50);
+
+  }
+  delay(2000);
+  pos=0;
+
+
 }
+
+
+
 
