e3aad59081ae04564b2e9575a59283b3e680dd3c
Ashish Mehrotra <ashishm1995@gmail.com>
2014-08-27 21:27:22 +0530

    Alternate Wheel Movement(Untested)
    Code for wheel movement in alternate directions

--- R_MotorDebug/R_MotorDebug.ino
+++ R_MotorDebug/R_MotorDebug.ino
@@ -36,6 +36,30 @@ void goAllForward(int m11,int m12,int m21,int m22,int analogSpeed)
       delay(200);
     }
 }
+//-------------------------FUNCTIONS FOR WHEEL MOVEMENT IN ALTERNATE DIRECTIONS------------------------------
+void goAlternate1(int m11,int m12,int m21,int m22,int analogSpeed)
+{
+   for(int i=0;i<15;i++)
+   {
+      analogWrite(m21,LOW);
+      digitalWrite(m22,analogSpeed);
+      analogWrite(m11,analogSpeed);
+      digitalWrite(m12,LOW);
+      delay(200);
+   } 
+}
+void goAlternate2(int m11,int m12,int m21,int m22,int analogSpeed)
+{
+   for(int i=0;i<15;i++)
+   {
+      analogWrite(m11,LOW);
+      digitalWrite(m12,analogSpeed);
+      analogWrite(m21,LOW);
+      digitalWrite(m22,analogSpeed);
+      delay(200);
+   } 
+}
+//----------------------------------------------------------------------------------------------------------
 void debug(int m11,int m12,int m21,int m22,int analogSpeed) //Keep m11 high and m12 low for forward
 {                                           //Keep m21 high and m22 low for forward
   Serial.println("Both forward");
@@ -43,5 +67,9 @@ void debug(int m11,int m12,int m21,int m22,int analogSpeed) //Keep m11 high and 
   delay(300);
   Serial.println("Both backward");
   goAllBackward(m11,m12,m21,m22,analogSpeed);
+  delay(300);
+  goAlternate1(m11,m12,m21,m22,analogSpeed);
+  delay(300);
+  goAlternate2(m11,m12,m21,m22,analogSpeed);
   delay(300);
 }
